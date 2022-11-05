565781fc171e74173034c239b86252d5fe29324d
root <root@DESKTOP-CBOU23K.localdomain>
2022-11-05 05:31:55 -0400

    implementing connect wallet

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -3,3 +3,25 @@ import React, { useState, useEffect } from 'react'
 export const TransactionContext = React.createContext()
 
 let eth
+
+if (typeof window !== 'undefined') {
+    eth = window.ethereum
+  }
+
+const TransactionProvider = ({children}) => {
+    const {currentAccount, setCurrentAccount} = useState()
+
+
+    const connectWallet = async (metamask = eth) => {
+
+        try {
+    
+            if (!metamask) return alert('Please install metamask')
+        
+        const accounts = await metamask.request({method: 'eth_requestAccounts' })
+        
+        setCurrentAccount(accounts[0])
+        
+    }
+   }
+}
